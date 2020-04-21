91e52ac14fa5c42a4ab96305e0e83a90b5cc11dd
k_vervainiotis <k.verveniotis17@gmail.com>
2020-04-21 17:44:20 +0300

    end ass

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -1,17 +1,20 @@
 package guru.springframework.bootstrap;
 
-import guru.springframework.domain.Category;
-import guru.springframework.domain.Recipe;
-import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.domain.*;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class RecipeBootstrap {
+@Component
+public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
     private final RecipeRepository recipeRepository;
@@ -21,6 +24,11 @@ public class RecipeBootstrap {
         this.categoryRepository = categoryRepository;
         this.recipeRepository = recipeRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        recipeRepository.saveAll(getRecipies());
     }
 
     private List<Recipe> getRecipies(){
@@ -62,6 +70,13 @@ public class RecipeBootstrap {
             throw new RuntimeException("Expected UOM Not Found!");
         }
 
+        UnitOfMeasure eachUop = eachUomOptional.get();
+        System.out.println("eachUop: " + eachUop.getDescription());
+        UnitOfMeasure tableSpoonUop = tableSpoonUomOptional.get();
+        UnitOfMeasure teaSpoonUop = teaSpoonUomOptional.get();
+        UnitOfMeasure PintUop = pintUomOptional.get();
+        UnitOfMeasure CupUop = cupUomOptional.get();
+        UnitOfMeasure dashUop = dashUomOptional.get();
 
         //Categories
         Optional<Category> AmericanCategoryOptional = categoryRepository.findByDescription("American");
@@ -70,22 +85,55 @@ public class RecipeBootstrap {
             throw new RuntimeException("Expected UOM Not Found!");
         }
 
-        Optional<Category> ItalianCategoryOptional = categoryRepository.findByDescription("Italian");
-
-        if (!ItalianCategoryOptional.isPresent()){
-            throw new RuntimeException("Expected Category Not Found!");
-        }
         Optional<Category> MexicanCategoryOptional = categoryRepository.findByDescription("Mexican");
 
         if (!MexicanCategoryOptional.isPresent()){
             throw new RuntimeException("Expected Category Not Found!");
         }
-        Optional<Category> FastFoodCategoryOptional = categoryRepository.findByDescription("Fast Food");
 
-        if (!FastFoodCategoryOptional.isPresent()){
-            throw new RuntimeException("Expected Category Not Found!");
-        }
+        Category americanCategory = AmericanCategoryOptional.get();
+        Category mexicanCategory = MexicanCategoryOptional.get();
 
+        //Guacamole
+        Recipe guacRecipe = new Recipe();
+        guacRecipe.setDescription("Perfect Guacamole");
+        guacRecipe.setPrepTime(10);
+        guacRecipe.setCookTime(0);
+        guacRecipe.setDifficulty(Difficulty.EASY);
+        guacRecipe.setServings(4);
+        guacRecipe.setDirections("1 Cut the avocado, remove flesh: Cut the avocados in half. Remove the pit. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon. (See How to Cut and Peel an Avocado.) Place in a bowl.\n" +
+                "\n" +
+                "2 Mash with a fork: Using a fork, roughly mash the avocado. (Don't overdo it! The guacamole should be a little chunky.)\n" +
+                "\n" +
+                "3 Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. The acid in the lime juice will provide some balance to the richness of the avocado and will help delay the avocados from turning brown.\n" +
+                "\n" +
+                "Add the chopped onion, cilantro, black pepper, and chiles. Chili peppers vary individually in their hotness. So, start with a half of one chili pepper and add to the guacamole to your desired degree of hotness.\n" +
+                "\n" +
+                "Remember that much of this is done to taste because of the variability in the fresh ingredients. Start with this recipe and adjust to your taste.\n" +
+                "\n" +
+                "Chilling tomatoes hurts their flavor, so if you want to add chopped tomato to your guacamole, add it just before serving.\n" +
+                "\n" +
+                "4 Serve: Serve immediately, or if making a few hours ahead, place plastic wrap on the surface of the guacamole and press down to cover it and to prevent air reaching it. (The oxygen in the air causes oxidation which will turn the guacamole brown.) Refrigerate until ready to serve.");
+        Notes guacNotes = new Notes();
+        guacNotes.setRecipeNotes("All you really need to make guacamole is ripe avocados and salt. After that, a little lime or lemon juice—a splash of acidity—will help to balance the richness of the avocado. Then if you want, add chopped cilantro, chiles, onion, and/or tomato.\n" +
+                "\n" +
+                "The trick to making perfect guacamole is using ripe avocados that are just the right amount of ripeness. Not ripe enough and the avocado will be hard and tasteless. Too ripe and the taste will be off.\n" +
+                "\n" +
+                "Check for ripeness by gently pressing the outside of the avocado. If there is no give, the avocado is not ripe yet and will not taste good. If there is a little give, the avocado is ripe. If there is a lot of give, the avocado may be past ripe and not good. In this case, taste test first before using.");
+        guacNotes.setRecipe(guacRecipe);
+        guacRecipe.setNotes(guacNotes);
+        guacRecipe.getIngredients().add(new Ingredient("ripe avocados", new BigDecimal(2), eachUop, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("salt", new BigDecimal(".25"), teaSpoonUop, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient(" fresh lime juice or lemon juice", new BigDecimal(2), tableSpoonUop, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("minced red onion or thinly sliced green onion", new BigDecimal(2), tableSpoonUop, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("serrano chiles, stems and seeds removed, minced", new BigDecimal(2), eachUop, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("cilantro", new BigDecimal(2), tableSpoonUop, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("freshly grated black pepper", new BigDecimal(1), dashUop, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("ripe tomato, seeds and pulp removed, chopped", new BigDecimal(".5"), eachUop, guacRecipe));
+
+        guacRecipe.getCategories().add(americanCategory);
+        guacRecipe.getCategories().add(mexicanCategory);
+        recipies.add(guacRecipe);
         return recipies;
     }
 }
--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,9 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.services.RecipeServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -15,22 +11,15 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepositorry;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeServiceImpl recipeService;
 
-    public IndexController(CategoryRepository categoryRepositorry, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepositorry = categoryRepositorry;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeServiceImpl recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-
-        Optional<Category> categoryOptional = categoryRepositorry.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat ID is: " + categoryOptional.get().getId());
-        System.out.println("Unit of Measure ID is: " + unitOfMeasureOptional.get().getId());
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
